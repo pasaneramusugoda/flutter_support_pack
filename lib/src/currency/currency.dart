d331900eafad1963a3895694bfc51397256477a3
Pasan Eramusugoda <vaprera91@gmail.com>
2020-11-25 13:41:36 +0530

    currency

---
+++ lib/src/currency/currency.dart
@@ -0,0 +1,99 @@
+import 'package:intl/intl.dart';
+
+class Currency {
+  static Currency _instance;
+  final NumberFormat _currency;
+  final NumberFormat _currencyCompact;
+  final NumberFormat _currencySimple;
+  final NumberFormat _currencyCompactSimple;
+
+  Currency._internal(this._currency, this._currencyCompact,
+      this._currencySimple, this._currencyCompactSimple) {
+    _instance = this;
+  }
+
+  static void init({
+    String locale,
+    String name = 'LKR',
+    String symbol = 'LKR ',
+    int decimalDigits = 2,
+  }) {
+    locale ??= Intl.defaultLocale;
+
+    final currency = NumberFormat.currency(
+      locale: locale,
+      name: name,
+      symbol: symbol,
+      decimalDigits: decimalDigits,
+    );
+
+    final currencyCompact = NumberFormat.compactCurrency(
+      locale: locale,
+      name: name,
+      symbol: symbol,
+      decimalDigits: decimalDigits,
+    );
+    final currencySimple = NumberFormat.simpleCurrency(
+      locale: locale,
+      name: name,
+      decimalDigits: decimalDigits,
+    );
+    final currencyCompactSimple = NumberFormat.compactSimpleCurrency(
+      locale: locale,
+      name: name,
+      decimalDigits: decimalDigits,
+    );
+
+    _instance = _instance ??
+        Currency._internal(
+            currency, currencyCompact, currencySimple, currencyCompactSimple);
+  }
+
+  static void _checkInstance() {
+    assert(
+      _instance != null,
+      '\nEnsure to initialize Currency before accessing it.'
+      '\nPlease execute the init method : Currency.init()',
+    );
+  }
+
+  static String format(num number) {
+    _checkInstance();
+    return _instance._currency.format(number);
+  }
+
+  static String formatCompact(num number) {
+    _checkInstance();
+    return _instance._currencyCompact.format(number);
+  }
+
+  static String formatSimple(num number) {
+    _checkInstance();
+    return _instance._currencySimple.format(number);
+  }
+
+  static String formatCompactSimple(num number) {
+    _checkInstance();
+    return _instance._currencyCompactSimple.format(number);
+  }
+
+  static num parse(String value) {
+    _checkInstance();
+    return _instance._currency.parse(value);
+  }
+
+  static num parseCompact(String value) {
+    _checkInstance();
+    return _instance._currencyCompact.parse(value);
+  }
+
+  static num parseSimple(String value) {
+    _checkInstance();
+    return _instance._currencySimple.parse(value);
+  }
+
+  static num parseCompactSimple(String value) {
+    _checkInstance();
+    return _instance._currencyCompactSimple.parse(value);
+  }
+}
