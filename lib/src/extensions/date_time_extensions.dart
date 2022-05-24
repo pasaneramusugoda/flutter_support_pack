f3ac9ead9ece6aa7ff41a02c83899c6c72258cc3
Pasan Eramusugoda <vaprera91@gmail.com>
2022-05-24 13:08:14 +0530

    refactor(date_time): set " " as default separator with document update

--- lib/src/extensions/date_time_extensions.dart
+++ lib/src/extensions/date_time_extensions.dart
@@ -6,55 +6,70 @@ extension DateTimeExtension on DateTime {
 
   //region date
   //region date month year
-  String toDMY([String? separator1, String? separator2]) =>
+  /// Default => 2022-05-24 => 24 5 2022
+  String toDMY([String? separator1 = ' ', String? separator2 = ' ']) =>
       DateFormat('d${separator1}M${separator1}y').format(this);
 
-  String toDdMmYy([String? separator1, String? separator2]) =>
+  /// Default => 2022-05-24 => 24 05 22
+  String toDdMmYy([String? separator1 = ' ', String? separator2 = ' ']) =>
       DateFormat('dd${separator1}MM${separator2}yy').format(this);
 
-  String toDdMmYyyy([String? separator1, String? separator2]) =>
+  /// Default => 2022-05-24 => 24 05 2022
+  String toDdMmYyyy([String? separator1 = ' ', String? separator2 = ' ']) =>
       DateFormat('dd${separator1}MM${separator2}yyyy').format(this);
 
-  String toDdMmmYy([String? separator1, String? separator2]) =>
+  /// Default => 2022-05-24 => 24 May 22
+  String toDdMmmYy([String? separator1 = ' ', String? separator2 = ' ']) =>
       DateFormat('dd${separator1}MMM${separator2}yy').format(this);
 
-  String toDdMmmYyyy([String? separator1, String? separator2]) =>
+  /// Default => 2022-05-24 => 24 May 2022
+  String toDdMmmYyyy([String? separator1 = ' ', String? separator2 = ' ']) =>
       DateFormat('dd${separator1}MMM${separator2}yyyy').format(this);
 
   //endregion
 
   //region month date year
-  String toMDY([String? separator1, String? separator2]) =>
+  /// Default => 2022-05-24 => 5 24 2022
+  String toMDY([String? separator1 = ' ', String? separator2 = ' ']) =>
       DateFormat('M${separator1}d${separator2}y').format(this);
 
-  String toMmDdYy([String? separator1, String? separator2]) =>
+  /// Default => 2022-05-24 => 05 24 22
+  String toMmDdYy([String? separator1 = ' ', String? separator2 = ' ']) =>
       DateFormat('MM${separator1}dd${separator2}yy').format(this);
 
-  String toMmDdYyyy([String? separator1, String? separator2]) =>
+  /// Default => 2022-05-24 => May 24 2022
+  String toMmDdYyyy([String? separator1 = ' ', String? separator2 = ' ']) =>
       DateFormat('MM${separator1}dd${separator2}yyyy').format(this);
 
-  String toMmmDdYy([String? separator1, String? separator2]) =>
+  /// Default => 2022-05-24 => May 24 22
+  String toMmmDdYy([String? separator1 = ' ', String? separator2 = ' ']) =>
       DateFormat('MMM${separator1}dd${separator2}yy').format(this);
 
-  String toMmmDdYyyy([String? separator1, String? separator2]) =>
+  /// Default => 2022-05-24 => May 05 2022
+  String toMmmDdYyyy([String? separator1 = ' ', String? separator2 = ' ']) =>
       DateFormat('MMM${separator1}dd${separator2}yyyy').format(this);
 
   //endregion
 
   //region year month date
-  String toYMD([String? separator1, String? separator2]) =>
+  /// Default => 2022-05-24 => 2022 5 24
+  String toYMD([String? separator1 = ' ', String? separator2 = ' ']) =>
       DateFormat('y${separator1}M${separator2}d').format(this);
 
-  String toYyMmDd([String? separator1, String? separator2]) =>
+  /// Default => 2022-05-24 => 22 05 24
+  String toYyMmDd([String? separator1 = ' ', String? separator2 = ' ']) =>
       DateFormat('yy${separator1}MM${separator2}dd').format(this);
 
-  String toYyyyMmDd([String? separator1, String? separator2]) =>
+  /// Default => 2022-05-24 => 2022 05 24
+  String toYyyyMmDd([String? separator1 = ' ', String? separator2 = ' ']) =>
       DateFormat('yyyy${separator1}MM${separator2}dd').format(this);
 
-  String toYyMmmDd([String? separator1, String? separator2]) =>
+  /// Default => 2022-05-24 => 22 May 24
+  String toYyMmmDd([String? separator1 = ' ', String? separator2 = ' ']) =>
       DateFormat('yy${separator1}MMM${separator2}dd').format(this);
 
-  String toYyyyMmmDd([String? separator1, String? separator2]) =>
+  /// Default => 2022-05-24 => 2022 May 24
+  String toYyyyMmmDd([String? separator1 = ' ', String? separator2 = ' ']) =>
       DateFormat('yyyy${separator1}MMM${separator2}dd').format(this);
 //endregion
 //endregion
